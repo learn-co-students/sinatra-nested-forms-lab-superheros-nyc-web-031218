76863f18d0b7e9032283027cf77cd5e2e383263d
victoria-huang <vctrah@gmail.com>
2018-04-04 13:45:32 -0400

    add superhero and team views, get/post methods in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,18 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+
+    # get '/teams' do
+    #   erb :team
+    # end
+
+    post '/teams' do
+      @team = params[:team]
+      # redirect "/teams"
+      erb :team
+    end
 
 end
